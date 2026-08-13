ee98749567976560769cd4ff9e05e39667724a03
Badal Chand <badalchand636@gmail.com>
2026-08-13 10:44:30 +0545

    Completed the Schema and dto for the contact form

---
+++ server/src/contact/dto/create-dto.ts
@@ -0,0 +1,40 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateContactDto {
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(100)
+  name!: string;
+
+
+  @IsEmail()
+  @IsNotEmpty()
+  email!: string;
+
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(30)
+  phone!: string;
+
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(150)
+  subject!: string;
+
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(10)
+  @MaxLength(2000)
+  message!: string;
+}
